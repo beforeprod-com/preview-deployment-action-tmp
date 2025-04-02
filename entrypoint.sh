2c17f2e777517c51a4bdcb34330e0e37c4706f79
Sven Hohlfeld <sho1975@gmail.com>
2025-04-02 15:10:44 +0200

    fix: capture and set URL output from shpr app start command

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@
 echo "shpr action starting..."
 echo "platform: $INPUT_PLATFORM"
 echo "build-folder: $INPUT_BUILD_FOLDER"
-/shpr app start $INPUT_PLATFORM $INPUT_BUILD_FOLDER
+
+# Capture the output of shpr app start and extract the URL
+URL=$(/shpr app start $INPUT_PLATFORM $INPUT_BUILD_FOLDER | grep -o 'https://[^[:space:]]*')
+echo "::set-output name=url::$URL"
+
 /shpr app list
 
+# Set the time output
+echo "::set-output name=time::$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
